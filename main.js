bdec6f775cf9bd4fae619da5b8566c25b0c01438
Jeroen <jaiwhnm@gmail.com>
2020-06-12 15:15:38 +0200

    Animation fixed

--- main.js
+++ main.js
@@ -23,9 +23,10 @@ function moveFirstBird() {
         if (listOfChildren[child].outerHTML && typeof listOfChildren[child] == 'object') {
             console.log("DIT IS EEN PATH: " + listOfChildren[child].outerHTML);
             // In dit gedeelte moet die animatie geregeld worden, ik zie dat jij daar al het één en ander voor had gemaakt in CSS. Ik zou dat ff checken als ik jou was.
-            setAttributes(listOfChildren[child], { 'transform': 'translate(-683.83 -56.32)', 'transition': 'transform 6s ease' });
+            setAttributes(listOfChildren[child], { 'transform': 'translate(-783.83 -56.32)' });
         }
     }
+    document.getElementById('bird1').style.animation = 'rotation 2s infinite ease-out';
 }
 
 $(document).keydown(function (e) {
