1d626d67d90a1d42b3af86a261e4d0bb9cf604ba
Jeroen <jaiwhnm@gmail.com>
2020-06-12 13:25:35 +0200

    Fixes in CSS
    - Zie main.js regel 25 (over animaties)

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 function moveAllBirds() {
-    $(".allBirds").click(function () {
+    $(".allBirds").animate({
+        left: '-100%'
+    }, 500, function () {
+        $(".allBirds").css('left', '150%');
         $(".allBirds").animate({
-            left: '-100%'
-        }, 500, function () {
-            $(".allBirds").css('left', '150%');
-            $(".allBirds").animate({
-                left: '0%'
-            }, 500);
-        });
+            left: '0%'
+        }, 500);
     });
 }
 
 function setAttributes(el, attrs) {
-    for(var key in attrs) {
-      el.setAttribute(key, attrs[key]);
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
     }
 }
 
@@ -23,12 +21,9 @@ function moveFirstBird() {
 
     for (var child in listOfChildren) {
         if (listOfChildren[child].outerHTML && typeof listOfChildren[child] == 'object') {
-            console.log("DIT IS EEN PATH" + listOfChildren[child]);
-            // setAttributes(listOfChildren[child], {'transform': 'translate(-183.83 -56.32)', 'transition': 'all 1.5s ease'});
-            listOfChildren[child].setAttribute('transform', 'translate(-183.83 -56.32)');
-            listOfChildren[child].animate({
-                transform: 'translate(-483.83 -56.32)'
-            }, 500);
+            console.log("DIT IS EEN PATH: " + listOfChildren[child].outerHTML);
+            // In dit gedeelte moet die animatie geregeld worden, ik zie dat jij daar al het één en ander voor had gemaakt in CSS. Ik zou dat ff checken als ik jou was.
+            setAttributes(listOfChildren[child], { 'transform': 'translate(-683.83 -56.32)', 'transition': 'transform 6s ease' });
         }
     }
 }
@@ -36,14 +31,7 @@ function moveFirstBird() {
 $(document).keydown(function (e) {
     switch (e.which) {
         case 32:    //Hele vogel vliegt weg wanneer op spacebar wordt geklikt
-            $(".allBirds").animate({
-                left: '-100%'
-            }, 500, function () {
-                $(".allBirds").css('left', '150%');
-                $(".allBirds").animate({
-                    left: '0%'
-                }, 500);
-            });
+            moveAllBirds();
             break;
     }
 });
