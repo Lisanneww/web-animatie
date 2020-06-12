9e129cb81161213a6cab56ad6f680eb33abb4203
Lisanne <64895677+Lisanneww@users.noreply.github.com>
2020-06-12 15:34:56 +0200

    Animation fixed

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
 function moveAllBirds() {
-    $(".allBirds").click(function () {
+    $(".allBirds").animate({
+        left: '-100%'
+    }, 500, function () {
+        $(".allBirds").css('left', '150%');
         $(".allBirds").animate({
-            left: '-100%'
-        }, 500, function () {
-            $(".allBirds").css('left', '150%');
-            $(".allBirds").animate({
-                left: '0%'
-            }, 500);
-        });
+            left: '0%'
+        }, 500);
     });
 }
 
 function setAttributes(el, attrs) {
-    for(var key in attrs) {
-      el.setAttribute(key, attrs[key]);
+    for (var key in attrs) {
+        el.setAttribute(key, attrs[key]);
     }
 }
 
 // Ik haal alle elementen op die in het <g> element zitten en ben op zoek naar alle <path> elementen zodat daaraan een transform/transition kan toekennen
-function moveFirstBird() {
-    var listOfChildren = document.getElementById('bird1').childNodes;
+function moveOneBird(bird) {
 
-    for (var child in listOfChildren) {
-        if (listOfChildren[child].outerHTML && typeof listOfChildren[child] == 'object') {
-            console.log("DIT IS EEN PATH" + listOfChildren[child]);
-            // setAttributes(listOfChildren[child], {'transform': 'translate(-183.83 -56.32)', 'transition': 'all 1.5s ease'});
-            listOfChildren[child].setAttribute('transform', 'translate(-183.83 -56.32)');
-            listOfChildren[child].animate({
-                transform: 'translate(-483.83 -56.32)'
-            }, 500);
-        }
+    switch (bird) {
+        case 1:
+            document.getElementById('bird1').style.animation = 'rotation 2s ease-out';
+            break;
+        case 2:
+            document.getElementById('bird2').style.animation = 'rotation 2s ease-out';
+            break;
+        case 3:
+            document.getElementById('bird3').style.animation = 'rotation 2s ease-out';
+            break;
+        case 4:
+            document.getElementById('bird4').style.animation = 'rotation 2s ease-out';
+            break;
+        case 5:
+            document.getElementById('bird5').style.animation = 'rotation 2s ease-out';
+            break;
+        case 6:
+            document.getElementById('bird6').style.animation = 'rotation 2s ease-out';
+            break;
+        case 7:
+            document.getElementById('bird7').style.animation = 'rotation 2s ease-out';
+            break;
+        case 8:
+            document.getElementById('bird8').style.animation = 'rotation 2s ease-out';
+            break;
     }
+
+
 }
 
 $(document).keydown(function (e) {
     switch (e.which) {
         case 32:    //Hele vogel vliegt weg wanneer op spacebar wordt geklikt
-            $(".allBirds").animate({
-                left: '-100%'
-            }, 500, function () {
-                $(".allBirds").css('left', '150%');
-                $(".allBirds").animate({
-                    left: '0%'
-                }, 500);
-            });
+            moveAllBirds();
             break;
     }
 });
